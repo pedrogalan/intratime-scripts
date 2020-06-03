b907942d92093a32464db20e617e3ac134129233
Pedro Galan <pedro.galan@piksel.com>
2020-06-03 16:00:54 +0100

    Adapt the script to the new API

--- get-current-status.sh
+++ get-current-status.sh
@@ -11,20 +11,18 @@ DATA_TIME_ZONE="Europe/Madrid"
 CURRENT_TIME_ZONE="Europe/London"
 
 # Sign in to get token
-TOKEN=$(curl -v -s  -H 'Host: panel.intratime.es' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-GB,en-US;q=0.7,en;q=0.3' -H 'Referer: https://panel.intratime.es/login/fichaje_web/'-H 'Upgrade-Insecure-Requests: 1' --data "user_EMAIL=$EMAIL&user_PIN=$PIN" --compressed 'https://panel.intratime.es/login/fichaje_web' 2>&1 | grep 'Set-Cookie' | tail -1 | sed -e 's/.*ci_session=\(.*\)\; expires.*/\1/')
+TOKEN=$(curl -XPOST -s --data "user=$EMAIL&pin=$PIN" 'https://newapi.intratime.es/api/user/login' 2>&1 | jq -r .USER_TOKEN)
 
-# Get status
-CURRENT_STATUS=$(curl -s -H 'Host: panel.intratime.es' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:66.0) Gecko/20100101 Firefox/66.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: en-GB,en-US;q=0.7,en;q=0.3' -H 'Referer: https://panel.intratime.es/login/fichaje_web' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'X-Requested-With: XMLHttpRequest' -H 'Cookie:  ci_session='$TOKEN';' --data-binary "iDisplayLength=1&iSortCol_0=3&sSortDir_0=desc" --compressed 'https://panel.intratime.es/login/list_datatables/' | jq .aaData[0])
+# Get time of last check
+WHEN=$(curl -s -H 'token: '$TOKEN 'https://newapi.intratime.es/api/user/clockings?last=true&type=0,1,2,3' | jq -r .[].INOUT_DATE)
 
-ACTION=$(echo $CURRENT_STATUS | tr -d '" ' | awk -F "," '{print $2}')
-WHEN=$(echo $CURRENT_STATUS | tr -d '"' | awk -F "," '{print $4}')
 WHEN_IN_SECONDS=$(TZ="$DATA_TIME_ZONE" date -j -f " %Y-%m-%d %H:%M:%S" "$WHEN" "+%s")
+
 NOW=$(TZ="$CURRENT_TIME_ZONE" date +%s)
 SECONDS_SINCE_LAST_ACTION=$(($NOW-$WHEN_IN_SECONDS))
 
-printf '%02d days, %02dh %02dm %02ds since you set status %s.\n' \
+printf '%02d days, %02dh %02dm %02ds since your last status change.\n' \
     $(($SECONDS_SINCE_LAST_ACTION/86400)) \
     $(($SECONDS_SINCE_LAST_ACTION%86400/3600)) \
     $(($SECONDS_SINCE_LAST_ACTION%3600/60)) \
-    $(($SECONDS_SINCE_LAST_ACTION%60)) \
-    $ACTION
+    $(($SECONDS_SINCE_LAST_ACTION%60))
